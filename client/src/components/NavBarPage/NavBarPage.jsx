ee5d0dce61ffae4af4a960f546d595157f1f6086
MichaelSobolev <alsm16.wm@gmail.com>
2021-12-14 17:23:04 +0300

    ADD: news route & update navbar

--- client/src/components/NavBarPage/NavBarPage.jsx
+++ client/src/components/NavBarPage/NavBarPage.jsx
@@ -1,27 +1,82 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./index.css";
 
 export const NavBarPage = (props) => {
-
-
   return (
     <header className="header">
-
-  
       <div className="sidenav">
-        <NavLink className="header__link" activeClassName="header__link_active" to="/">findATable</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/announcements">Найти игру</ NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/user-page">Профиль</ NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/logout">Выйти</ NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/eror">Error</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/login">Войти</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/logout">выйти</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/postcard">CARD</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/announcements/new">Новый Пост</NavLink>
-        <NavLink className="header__link" activeClassName="header__link_active" to="/admin">Админ</NavLink>        
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/"
+        >
+          findATable
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/announcements"
+        >
+          Найти игру
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/user-page"
+        >
+          Профиль
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/logout"
+        >
+          Выйти
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/eror"
+        >
+          Error
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/login"
+        >
+          Войти
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/logout"
+        >
+          выйти
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/news"
+        >
+          Новости
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/announcements/new"
+        >
+          Новый Пост
+        </NavLink>
+        <NavLink
+          className="header__link"
+          activeClassName="header__link_active"
+          to="/admin"
+        >
+          Админ
+        </NavLink>
       </div>
     </header>
-  )
-}
-
+  );
+};
